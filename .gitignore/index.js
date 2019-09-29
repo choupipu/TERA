459f035f71da46f9c91046dabd53615c958a6f90
choupipu <55955636+choupipu@users.noreply.github.com>
2019-09-29 19:53:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
 bot.on('ready', function () {
   console.log("Je suis connecté !")
-});
+})
 
 bot.on('message', message => {
-  if (message.content === "!ping") {
-    message.reply('pong !')
-  }
-});
+    if (message.content === '!ping') {
+      message.reply('pong !')
+    }
+  })
 
-bot.login("NjI3NDQwODcyNzQ5MDA2ODQ4.XZDp0w.3JcwrN3v97-0bGmDJGDzXDeGaqQ");
+bot.login('process.env.TOKEN')
