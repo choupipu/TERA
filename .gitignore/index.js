281bd884c3553d7e49b0619a6cbc329a58c5e76c
choupipu <55955636+choupipu@users.noreply.github.com>
2019-09-29 21:02:37 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('ready', function () {
+tera.on('ready', function () {
   console.log("Je suis connecté !")
 });
 
